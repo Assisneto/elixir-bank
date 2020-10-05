4073bd628fb2ea0b280d1784afd9a52b9d78f955
Assisneto Damasceno <netodamasceno12@gmail.com>
2020-10-05 08:37:02 -0300

    transfer function

--- lib/account.ex
+++ lib/account.ex
@@ -1,4 +1,23 @@
 defmodule Account do
-    defstruct user: User, balance: nil
+    defstruct user: User, balance: 1000
     def registration(user), do: %__MODULE__{user: user, balance: 1000}
+
+    def transfer(accounts, from, to, value) do
+     from = find_account(accounts,from)
+
+      cond do
+
+        balance_validation(from.balance, value) -> {:error, "insufficient funds"}
+
+        true ->
+          to = find_account(accounts, to)
+          from = %Account{from | balance: from.balance - value}
+          to = %Account{to | balance: to.balance + value}
+
+        [from, to]
+      end
+    end
+
+    defp balance_validation(balance, value), do: balance < value
+    defp find_account(accounts, account), do: Enum.find(accounts, &(&1.user.email == account.user.email))
 end
