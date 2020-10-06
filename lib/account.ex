b0a153cccfc90d1ea0811cd17e806e876c3fc0e5
Assisneto Damasceno <netodamasceno12@gmail.com>
2020-10-05 22:20:03 -0300

    add data persistence using file system

--- lib/account.ex
+++ lib/account.ex
@@ -1,33 +1,65 @@
 defmodule Account do
     defstruct user: User, balance: 1000
-    def registration(user), do: %__MODULE__{user: user, balance: 1000}
+    @accounts "accounts.txt"
+    def registration(user) do
 
-    def transfer(accounts, from, to, value) do
-     from = find_account(accounts,from)
+      case find_account(user) do
+        nil ->
+         [%__MODULE__{user: user}] ++ get_accounts()
+         |> set_accounts
 
+        _ -> {:error, "account already exists"}
+      end
+
+    end
+    def transfer(from, to, value) do
+     from = find_account(from.user)
       cond do
-
         balance_validation(from.balance, value) -> {:error, "insufficient funds"}
-
         true ->
-          to = find_account(accounts, to)
+          accounts = get_accounts()
+
+          accounts = List.delete accounts, from
+          accounts = List.delete accounts, to
+
           from = %Account{from | balance: from.balance - value}
           to = %Account{to | balance: to.balance + value}
 
-        [from, to]
+          accounts = accounts ++ [from, to]
+
+          set_accounts(accounts)
+
+          {:ok, from ,to , "Sucess"}
       end
     end
 
     def withdraw(account, value) do
       cond do
         balance_validation(account.balance, value) -> {:error, "insufficient funds"}
-
         true ->
-          %Account{account | balance: account.balance - value}
+          accounts = get_accounts()
+          accounts = List.delete accounts, account
+
+          account = %Account{account | balance: account.balance - value}
+          accounts = accounts ++ account
+
+          set_accounts(accounts)
+          {:ok, account, "Sucess"}
       end
 
     end
 
     defp balance_validation(balance, value), do: balance < value
-    defp find_account(accounts, account), do: Enum.find(accounts, &(&1.user.email == account.user.email))
+
+    defp find_account(user), do: Enum.find(get_accounts(), &(&1.user.email == user.email))
+
+    defp get_accounts do
+      {:ok, binary} = File.read(@accounts)
+      :erlang.binary_to_term(binary)
+    end
+
+    defp set_accounts (accounts) do
+      binary_accounts = :erlang.term_to_binary(accounts)
+      File.write(@accounts,  binary_accounts)
+    end
 end
